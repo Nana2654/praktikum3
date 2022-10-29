4ac79ef63b012cc29c778239bff2da81a00ee9b1
Nana2654 <suryana2653@gmail.com>
2022-10-29 16:59:55 +0700

    menambahkan file

---
+++ menghitung luas keliling lingkaran.py
@@ -0,0 +1,7 @@
+r = int(input("masukan jari jari (r) : "))
+
+l = float(22) / 7 * r * r
+k =2 * (float(22) / 7) * r
+
+print("luas (l) lingkaran : " + str(l))
+print("keliling (k) lingkaran : " + str(k))
